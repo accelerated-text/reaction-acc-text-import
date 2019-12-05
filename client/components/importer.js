3b869d53fe675963ac109325c758e7aa71cf3b04
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-12-05 10:59:15 +0200

    Hardcoded at the moment

--- client/components/importer.js
+++ client/components/importer.js
@@ -7,7 +7,7 @@ class Importer extends Component {
   constructor(props) {
     super(props);
     this.state = {descriptionType: "books"};
-    this.accTextUrl = "http://localhost:3000/nlg/";
+    this.accTextUrl = "http://localhost:3001/nlg";
     
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,10 +18,29 @@ class Importer extends Component {
     return this.state;
   };
 
+  readResult = resultId => {
+    console.log(`Reading data from ${resultId}`);
+    fetch(`${this.accTextUrl}/${resultId}`, {method: "get"})
+      .then(response => response.json())
+      .then(body => {
+            if(body.ready){
+              console.log(body);
+            }
+            else{
+              console.log("NLG result is not ready yet. Retry after second")
+              setTimeout(() => {
+                this.readResult(resultId);
+              }, 1000);
+            }
+      });
+    
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     const { data, descriptionType } = this.state;
-    const request = { documentPlanId: descriptionType, dataId: "" };
+    const rows = {testProduct: {title: "Misery", author: "Stephen King"}};
+    const request = { documentPlanId: "1d1f6f14-fb26-4ebb-83f2-26e4b2e00a0b", dataRows: rows, readerFlagValues: {} };
     console.log(request);
     const conf = {
       method: "post",
@@ -29,9 +48,9 @@ class Importer extends Component {
       headers: new Headers({ "Content-Type": "application/json",})
     };
     
-    fetch(this.accTextUrl, conf)
+    fetch(`${this.accTextUrl}/_bulk/`, conf)
        .then(response => response.json())
-       .then(result => console.log(result));
+       .then(result => this.readResult(result.resultId));
   };
   
   render(){
