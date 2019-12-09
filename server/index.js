ac435075845f0cc16aaf94482b2bc112d84f1409
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-12-09 09:18:12 +0200

    Cleanup

--- server/index.js
+++ server/index.js
@@ -45,69 +45,6 @@ Meteor.methods({
         publishProductToCatalogById([decodedId], getContext());
 
         return true;
-        // const product = Products.findOne({ _id: decodedId });
-        // if (!product) {
-        //     throw new ReactionError("not-found", `Product id: "${decodedId}" not found`);
-        // }
-
-        // const handle = createHandle(Reaction.getSlug(data.title), decodedId);
-
-
-        // const update = {
-        //     "title": data.title,
-        //     "isVisible": true,
-        //     handle
-        // };
-
-        // const result = Products.update({_id: decodedId}, {$set: update}, {selector: "simple"});
    }
 });
 
-// export default uploadProducts = (products) => {
-//     /*
-//       * Single Product example:
-
-//       ```json
-//       {
-// 	    "_id": "h2v7MYCXBtjMm7Piv",
-// 	    "type": "simple",
-//       "supportedFulfillmentTypes": ["shipping"],
-// 	    "ancestors": [],
-// 	    "shopId": "J8Bhq3uTtdgwZx3rz",
-// 	    "title": "Reaction Bag of Beans",
-// 	    "originCountry": "US",
-// 	    "requiresShipping": true,
-// 	    "handle": "reaction-bag-of-beans",
-// 	    "isDeleted": false,
-// 	    "isVisible": true,
-// 	    "template": "productDetailSimple",
-// 	    "price": {
-// 		  "range": "12 - 24",
-// 		  "min": 12.00,
-// 		  "max": 24.00
-// 	    },
-// 	    "workflow": {
-// 		  "status": "new"
-// 	    },
-// 	    "isLowQuantity": false,
-// 	    "isSoldOut": true,
-// 	    "vendor": "Bluestone Coffee",
-// 	    "description": "A bag of rich Arabica coffee. Great for a fueling up for a night out or for settling in for a night of coding at home",
-// 	    "isBackorder": true,
-// 	    "hashtags": [
-// 		  "hZdSevCBP8dDc66Ba"
-// 	    ]
-//       },
-//       ```
-//       */
-//     Logger.info("Starting load Products");
-//     products.forEach((product) => {
-//         product.createdAt = new Date();
-//         product.updatedAt = new Date();
-//         Products.insert(product, {}, { publish: true });
-//         if (product.type === "simple" && product.isVisible) {
-//             publishProductToCatalogById(product._id, getContext());
-//         }
-//     });
-//     Logger.info("Products loaded");
-// }
