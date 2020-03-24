a1418016b226671f08d30b6943e80baac93fd72d
Sarunas Navickas <zaibacu@gmail.com>
2020-03-24 14:59:36 +0200

    Less problems with Png image

--- client/components/logo.js
+++ client/components/logo.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export class Logo extends Component {
     render(){
-        return (<img src="https://raw.githubusercontent.com/tokenmill/accelerated-text/master/front-end/assets/accelerated-text-logo.svg" alt="Accelerated Text Logo"/>);
+        return (<img src="https://raw.githubusercontent.com/tokenmill/accelerated-text/master/docs/assets/accelerated-text-logo.png" alt="Accelerated Text Logo"/>);
     }
 };
